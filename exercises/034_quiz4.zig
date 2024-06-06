a9a815ebf9babe2bb60638d0b70c93f7f5bc9714
CeduDev <cedric.neergaard@gmail.com>
2024-06-06 22:53:30 +0300

    complete 3-36

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -9,7 +9,7 @@ const std = @import("std");
 
 const NumError = error{IllegalNumber};
 
-pub fn main() void {
+pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
     const my_num: u32 = getNumber();
@@ -18,7 +18,6 @@ pub fn main() void {
 }
 
 // This function is obviously weird and non-functional. But you will not be changing it for this quiz.
-fn getNumber() NumError!u32 {
-    if (false) return NumError.IllegalNumber;
+fn getNumber() u32 {
     return 42;
 }
